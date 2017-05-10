ea4f01292b8bed298ed5409cffd40357cae052e0
jackyyyy <15958043011@163.com>
2017-05-10 16:18:40 +0800

    aa

--- javascripts/fun.js
+++ javascripts/fun.js
@@ -13,13 +13,13 @@ $(function() {
 			start = setInterval(function() {
 				which = number[Math.ceil(Math.random() * (number.length-1))];
 				$(".choise-number").html(which);
-				$(".choise-number").css({
-                    "background": "rgba("+ Math.ceil(Math.random() * 255) +","+ Math.ceil(Math.random() * 255) +","+ Math.ceil(Math.random() * 255) +","+1+")"
-				});
+				// $(".choise-number").css({
+    //                 "background": "rgba("+ Math.ceil(Math.random() * 255) +","+ Math.ceil(Math.random() * 255) +","+ Math.ceil(Math.random() * 255) +","+1+")"
+				// });
 				$("<div class='circle'></div>").html("<span>"+which+"</span>").css({
 					"top": Math.ceil(Math.random() * $(document).height()),
 					"left": Math.ceil(Math.random() * $(document).width()),
-                    "background": "rgba("+ Math.ceil(Math.random() * 255) +","+ Math.ceil(Math.random() * 255) +","+ Math.ceil(Math.random() * 255) +","+.5+")"
+                    // "background": "rgba("+ Math.ceil(Math.random() * 255) +","+ Math.ceil(Math.random() * 255) +","+ Math.ceil(Math.random() * 255) +","+.5+")"
 				}).appendTo("body").fadeIn("slow", function() {
 					$(this).fadeOut("slow", function() {
 						$(this).remove();
